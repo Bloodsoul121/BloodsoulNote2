f8e7656703c6cd584b7972578715bd6a449223f0
bloodsoul <812566160@qq.com>
2019-11-12 09:58:32 +0800

    commit

--- app/src/main/java/com/example/cgz/bloodsoulnote2/intent/IntentActivity.java
+++ app/src/main/java/com/example/cgz/bloodsoulnote2/intent/IntentActivity.java
@@ -1,8 +1,11 @@
 package com.example.cgz.bloodsoulnote2.intent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 
 import com.example.cgz.bloodsoulnote2.R;
 
@@ -17,5 +20,10 @@ public class IntentActivity extends AppCompatActivity {
     public void clickBtn1(View view) {
 //        GoToScoreUtils.goToMarket(this, getPackageName());
         GoToScoreUtils.goToMarket(this, "com.cwj.hsing");
+    }
+
+    public void clickBtn2(View view) {
+        Intent phoneIntent = new Intent("android.intent.action.CALL", Uri.parse("tel:" + "4006109993"));
+        startActivity(phoneIntent);
     }
 }
